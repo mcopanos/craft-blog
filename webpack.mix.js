e28edcff4d22c2ac4b99e15dbef3e07e7b1b3d9f
Mike Copanos <mcopanos@gmail.com>
2020-02-08 10:19:29 -0600

    build systems update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,13 @@
 const mix = require('laravel-mix');
 
 
-mix
+mix.version()
+    .disableNotifications()
+    .setPublicPath('web')
     .js('assets/js/main.js', 'web/js/main.js')
     .sass('assets/sass/app.scss', 'web/css/')
+    .sourceMaps()
+    .browserSync({
+        proxy: 'localhost:8080',
+        files: ['templates/**/*', 'web/css/*.css', 'web/js/*.js']
+    })

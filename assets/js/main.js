218b10fa540f91214fb1ec2bfd9a9fdbbcefcb7a
Mike Copanos <mcopanos@gmail.com>
2020-04-28 19:14:05 -0500

    add dynamic alt text to images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const hambugerMenu = document.querySelector('#js-mobile-nav');
 const mobileNav = document.querySelector('.collapse');
 
+// Mobile Nav function
 function openMenu() {
    mobileNav.classList.toggle('collapse');
 }
